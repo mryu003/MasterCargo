0e24909ce404f77b26a648d708e4e7d24a275b71
mryu003 <mryu003@ucr.edu>
2024-11-01 12:07:10 -0700

    Added home page and login page

--- main.py
+++
@@ -1,9 +0,0 @@
-# pip install flask
-# pip install flask-sqlalchemy 
-
-from website import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True) #Turn this to false when turning it in
