b3c8d81467f6c6a4ba04050a161bc6c137a39e45
deet5 <idenismiller@gmail.com>
2024-11-11 22:13:10 -0800

    add download function for log file

--- website/main.py
+++ website/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import time
+import os
 
 app = Flask(__name__)
 
@@ -17,6 +19,14 @@ def load():
 @app.route('/balance')
 def balance():
     return render_template('balance.html')
+
+@app.route('/download')
+def download_log():
+    curr_year = time.strftime("%Y")
+    file_name = "KeoghsPort" + curr_year + ".txt"
+    log_files = os.path.join(app.static_folder, 'log_files')
+    return send_from_directory(log_files, file_name, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)  #Change this to false when we turn in
