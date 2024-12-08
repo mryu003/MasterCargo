8217420a5d45834f88f95cbbcb3ab9cc2b5decdc
deet5 <idenismiller@gmail.com>
2024-12-07 20:26:32 -0800

    added balance operation

--- website/classes.py
+++ website/classes.py
@@ -11,6 +11,7 @@ LOAD = 'LOAD'
 UNLOAD = 'UNLOAD'
 MOVE = 'MOVE'
 START = 'START'
+BALANCE_DIFF = 0.1
 
 
 def get_ship_grid(file_path: str) -> list:
@@ -27,19 +28,39 @@ def get_ship_grid(file_path: str) -> list:
 def manhattan_distance(start: list, end: list) -> int:
     return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
+def get_balance_diff(ship_grid: list) -> float:
+        left = 0
+        right = 0
+
+        for row in range(MAX_ROW):
+            for col in range(MAX_COL):
+                if col < MAX_COL // 2:
+                    left += ship_grid[row][col].weight
+                else:
+                    right += ship_grid[row][col].weight
+
+        abs_diff = abs(left - right)
+        total_weight = left + right
+
+        return abs_diff / total_weight
+
 class Container:
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
 
 class Node:
-    def __init__(self, op: str, name: str, weight: int, from_pos: list, to_pos: list, time: int):
+    def __init__(self, op: str, name: str, weight: int, from_pos: list, to_pos: list, time: int, prev_grid: list, ship_grid: list, fval: int, step: int):
         self.op = op
         self.name = name
         self.weight = weight
         self.from_pos = from_pos
         self.to_pos = to_pos
         self.time = time
+        self.prev_grid = prev_grid
+        self.ship_grid = ship_grid
+        self.fval = fval
+        self.step = step
 
 class Transfer(Node):
     def __init__(self, 
@@ -55,13 +76,9 @@ class Transfer(Node):
                  unload_containers: list,
                  fval: int,
                  step: int):
-        super().__init__(op, name, weight, from_pos, to_pos, time)
-        self.prev_grid = prev_grid
-        self.ship_grid = ship_grid
+        super().__init__(op, name, weight, from_pos, to_pos, time, prev_grid, ship_grid, fval, step)
         self.load_containers = load_containers
         self.unload_containers = unload_containers
-        self.fval = fval
-        self.step = step
 
     def generate_children(self):
         children = []
@@ -74,18 +91,18 @@ class Transfer(Node):
                 new_ship_grid[top_x][top_y] = Container(UNUSED, 0)
 
                 children.append(Transfer(
-                                    MOVE, 
-                                    self.ship_grid[top_x][top_y].name, 
-                                    self.ship_grid[top_x][top_y].weight, 
-                                    [top_x, top_y], 
-                                    [new_loc_x, new_loc_y], 
-                                    time,
-                                    self.ship_grid,
-                                    new_ship_grid,
-                                    self.load_containers,
-                                    self.unload_containers,
-                                    0,
-                                    self.step + 1
+                                    op=MOVE, 
+                                    name=self.ship_grid[top_x][top_y].name, 
+                                    weight=self.ship_grid[top_x][top_y].weight, 
+                                    from_pos=[top_x, top_y], 
+                                    to_pos=[new_loc_x, new_loc_y], 
+                                    time=time,
+                                    prev_grid=self.ship_grid,
+                                    ship_grid=new_ship_grid,
+                                    load_containers=self.load_containers,
+                                    unload_containers=self.unload_containers,
+                                    fval=0,
+                                    step=self.step + 1
                                     ))
             else:
                 time = self.__unload_time([x, y])
@@ -94,18 +111,18 @@ class Transfer(Node):
                 new_unload_containers = copy.deepcopy(self.unload_containers)
                 new_unload_containers.remove([x, y])
                 children.append(Transfer(
-                                    UNLOAD, 
-                                    self.ship_grid[x][y].name, 
-                                    self.ship_grid[x][y].weight, 
-                                    [x, y], 
-                                    TRUCK, 
-                                    time,
-                                    self.ship_grid,
-                                    new_ship_grid,
-                                    self.load_containers,
-                                    new_unload_containers,
-                                    0,
-                                    self.step + 1
+                                    op=UNLOAD,
+                                    name=self.ship_grid[x][y].name,
+                                    weight=self.ship_grid[x][y].weight,
+                                    from_pos=[x, y],
+                                    to_pos=SHIP_IN_OUT,
+                                    time=time,
+                                    prev_grid=self.ship_grid,
+                                    ship_grid=new_ship_grid,
+                                    load_containers=self.load_containers,
+                                    unload_containers=new_unload_containers,
+                                    fval=0,
+                                    step=self.step + 1
                                     ))
 
         for container in self.load_containers:
@@ -117,18 +134,19 @@ class Transfer(Node):
                 new_load_containers = copy.deepcopy(self.load_containers)
                 new_load_containers.pop(0)
                 children.append(Transfer(
-                                    LOAD, 
-                                    container.name, 
-                                    container.weight, 
-                                    TRUCK, 
-                                    free_loc, 
-                                    time,
-                                    self.ship_grid,
-                                    new_ship_grid,
-                                    new_load_containers,
-                                    self.unload_containers,
-                                    0,
-                                    self.step + 1))
+                                    op=LOAD,
+                                    name=container.name,
+                                    weight=container.weight,
+                                    from_pos=TRUCK,
+                                    to_pos=free_loc,
+                                    time=time,
+                                    prev_grid=self.ship_grid,
+                                    ship_grid=new_ship_grid,
+                                    load_containers=new_load_containers,
+                                    unload_containers=self.unload_containers,
+                                    fval=0,
+                                    step=self.step + 1
+                                    ))
 
         return children
     
@@ -172,12 +190,67 @@ class Transfer(Node):
         return manhattan_distance(SHIP_IN_OUT, dest)
     
 
+class Balance(Node):
+    def __init__(self, op: str, name: str, weight: int, from_pos: list, to_pos: list, time: int, prev_grid: list, ship_grid: list, fval: int, step: int, weight_diff: int):
+        super().__init__(op, name, weight, from_pos, to_pos, time, prev_grid, ship_grid, fval, step)
+        self.weight_diff = weight_diff
 
+    def generate_children(self, moves: list) -> list:
+        children = []
+        for source, dest in moves:
+            new_ship_grid = copy.deepcopy(self.ship_grid)
+            new_ship_grid[dest[0]][dest[1]] = self.ship_grid[source[0]][source[1]]
+            new_ship_grid[source[0]][source[1]] = Container(UNUSED, 0)
+            new_balance = get_balance_diff(new_ship_grid)
+            time = manhattan_distance(source, dest)
+            penaty = self.__penalty(new_ship_grid)
+            fval = self.step + 1 + time + abs(new_balance - BALANCE_DIFF) + penaty
+            children.append(Balance(
+                                op=MOVE,
+                                name=self.ship_grid[source[0]][source[1]].name,
+                                weight=self.ship_grid[source[0]][source[1]].weight,
+                                from_pos=source,
+                                to_pos=dest,
+                                time=time,
+                                prev_grid=self.ship_grid,
+                                ship_grid=new_ship_grid,
+                                fval=fval,
+                                step=self.step + 1,
+                                weight_diff=new_balance
+                                ))
+            
+        return children
+    
+    def __penalty(self, grid: list) -> int:
+        total_weight = sum(container.weight for row in grid for container in row if (container.name != UNUSED and container.name != NAN))
+        left_weight = sum(container.weight for row in grid for container in row[:MAX_COL//2] if (container.name != UNUSED and container.name != NAN))
+        right_weight = total_weight - left_weight
+
+        penalty = 0
+
+        for row in grid:
+            for container in row[:MAX_COL//2]:
+                penalty += container.weight * (1 if left_weight > right_weight else -1)
+            for container in row[MAX_COL//2:]:
+                penalty += container.weight * (1 if right_weight > left_weight else -1)
+
+        return penalty
+    
+    def __hash__(self):
+        return hash(str(self.from_pos[0]) + str(self.from_pos[1]) + self.name + str(self.weight) + str(self.time) + str(self.to_pos[0]) + str(self.to_pos[1]))
+    
+    def __eq__(self, other):
+        pos = self.from_pos == other.from_pos
+        name = self.name == other.name
+        weight = self.weight == other.weight
+        time = self.time == other.time
+        return pos and name and weight and time
+    
 class Ship:
     def __init__(self, ship_grid: list):
         self.ship_grid = ship_grid
         self.open_set = []
-        self.closed_set = []
+        self.closed_set = set()
 
     def get_transfer_steps(self, load_containers: list, unload_containers: list) -> list:
         if not self.has_space(load_containers, unload_containers):
@@ -188,18 +261,18 @@ class Ship:
         came_from = {}
 
         start_node = Transfer(
-            START, 
-            NAN, 
-            0, 
-            TRUCK, 
-            TRUCK, 
-            0,
-            start,
-            start,
-            load_containers,
-            unload_containers,
-            0,
-            0
+            op=START,
+            name=NAN,
+            weight=0,
+            from_pos=TRUCK,
+            to_pos=TRUCK,
+            time=0,
+            prev_grid=start,
+            ship_grid=start,
+            load_containers=load_containers,
+            unload_containers=unload_containers,
+            fval=0,
+            step=0
         )
 
         self.open_set.append(start_node)
@@ -208,7 +281,7 @@ class Ship:
             self.open_set.sort(key=lambda x: x.fval)
             curr_node = self.open_set.pop(0)
             curr_node.fval = curr_node.step + curr_node.time
-            self.closed_set.append(curr_node)
+            self.closed_set.add(curr_node)
 
             curr_goal = len(curr_node.load_containers) + len(curr_node.unload_containers)
             if curr_goal == goal:
@@ -216,12 +289,73 @@ class Ship:
             
             children = curr_node.generate_children()
             for child in children:
-                if child in self.closed_set:
+                if any(child.ship_grid == node.ship_grid for node in self.closed_set):
                     continue
                 self.open_set.append(child)
                 came_from[child] = curr_node
 
         return None
+    
+    def get_balance_steps(self) -> list:
+        start = self.ship_grid
+        curr_balance = get_balance_diff(self.ship_grid)
+        came_from = {}
+
+        start_node = Balance(
+            op=START,
+            name=NAN,
+            weight=0,
+            from_pos=TRUCK,
+            to_pos=TRUCK,
+            time=0,
+            prev_grid=start,
+            ship_grid=start,
+            fval=0,
+            step=0,
+            weight_diff=curr_balance
+        )
+
+        self.open_set.append(start_node)
+
+        while self.open_set:
+            self.open_set.sort(key=lambda x: x.fval)
+            
+            curr_node = self.open_set.pop(0)
+
+            self.closed_set.add(curr_node)
+
+            if curr_node.weight_diff <= BALANCE_DIFF:
+                return self.__get_path(came_from, curr_node)
+            
+            moves = self.__get_possible_moves(curr_node.ship_grid)
+            children = curr_node.generate_children(moves)
+            for child in children:
+                if child in self.closed_set: 
+                    continue
+                self.open_set.append(child)
+                came_from[child] = curr_node
+
+        return None
+    
+    def __get_possible_moves(self, ship_grid: list) -> list:
+        moves = []
+        for row in range(MAX_ROW):
+            for col in range(MAX_COL):
+                if ship_grid[row][col].name != UNUSED and ship_grid[row][col].name != NAN and (row == MAX_ROW - 1 or ship_grid[row+1][col].name == UNUSED):
+                    dests = self.__get_dest(ship_grid, [row, col])
+                    for dest in dests:
+                        moves.append(([row, col], dest))
+
+        return moves
+    
+    def __get_dest(self, ship_grid: list, source: list) -> list:
+        dests = []
+        for row in range(MAX_ROW):
+            for col in range(MAX_COL):
+                if ship_grid[row][col].name == UNUSED and (row == 0 or ship_grid[row - 1][col].name != UNUSED) and ship_grid[row - 1][col].name != ship_grid[source[0]][source[1]].name:
+                    dests.append([row, col])
+        
+        return dests
 
     def __get_path(self, came_from: dict, curr_node: Transfer) -> list:
         path = []
